aae453636773b7613abdfb88d693a4982d97db3f
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-05 20:30:29 +0600

    mngoose connect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        MONGODB_URL: "mongodb+srv://rokibulhasan018722:rokib123@cluster0.3htufth.mongodb.net/NEXT_JS_PRACTICE?retryWrites=true&w=majority&appName=Cluster0",
+    },
+};
 
 export default nextConfig;
