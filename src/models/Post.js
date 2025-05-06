f59d2b68766ee28d70eb8e3a2a165ac3f00128a4
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-06 22:59:43 +0600

    feat: post model done

---
+++ src/models/Post.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+export default mongoose.models.Post || mongoose.model("Post", postSchema);
